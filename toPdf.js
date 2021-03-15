ea22a99a194fea4eb1927fd45783c96f73d1718c
Omar Talbi <48894149+Omar-Talbi@users.noreply.github.com>
2021-03-15 19:47:34 +0100

    Create toPdf.js

---
+++ toPdf.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById("pdfButton")
+        .addEventListener("click", () => {
+            let delt = document.getElementsByClassName("fc-header-toolbar fc-toolbar fc-toolbar-ltr")[0];
+            delt.style.visibility = "hidden";
+            const calendar = this.document.getElementById("cal");
+            let titre = document.getElementsByTagName("h2")[0];
+            calendar.appendChild(titre);
+            calendar.insertBefore(titre, calendar.firstChild);
+            console.log(calendar);
+            console.log(window);
+   
+}
